cc5b0fe6ec1685b368ef020e111c242cec742507
harkeju <61701308+harkeju@users.noreply.github.com>
2022-06-22 12:57:53 +0100

    first commit

---
+++ myself.js
@@ -0,0 +1,7 @@
+let myDetails = {
+  Name: "Babatunde Akeju",
+  Height: "1.78 meters",
+  Country: "Nigeria",
+};
+console.log("I'm glad to share a little detail about me in the table below:");
+console.table(myDetails);
